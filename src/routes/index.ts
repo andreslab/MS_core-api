0680ce3f74e409d1d7e3909942704a0f61c8aac2
Jaime Andrade <jaimeandrade454@gmail.com>
2021-03-31 02:24:52 -0700

    sample request post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { indexController } from "../controller/indexController";
+import { indexController } from "../controller/indexControllerGet";
+import { indexControllerPost } from "../controller/indexControllerPost";
 
 const router: Router = Router();
 
 router.get('/', indexController.getInfo);
-router.get('/info', indexController.getInfoFromOtherModule);
-router.post('/infodb', indexController.getInfoFromDB);
+router.get('/info:name', indexController.getInfoFromOtherModule);
+router.get('/infodb', indexController.getInfoFromDB);
+
+router.post('/save', indexControllerPost.saveInfo);
+router.post('/saveinfo/:name', indexControllerPost.saveInfoFromOtherModule);
+router.post('/saveinfodb', indexControllerPost.saveInfoFromDB);
 
 router.get('/sammy', (req, res) => {
     res.send('Bienvenido a Sammy');
